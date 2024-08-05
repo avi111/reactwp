92d64e806737bd5ca3e5d6805caf8b80d3d21587
Avi Levkovich <avi111@gmail.com>
2024-08-05 21:50:32 +0300

    useApp

--- src/Language/useApp.ts
+++ src/Language/useApp.ts
@@ -1,5 +1,5 @@
 import { InjectedProps, Menus, SiteOptions } from "../types/types.ts";
-import { createContext, useCallback, useContext } from "react";
+import { createContext, useContext, useMemo } from "react";
 
 export const AppContext = createContext<InjectedProps>(window.object);
 
@@ -7,44 +7,43 @@ export const useApp = () => {
   const { query, site, content, translations, post, menus } =
     useContext(AppContext);
 
-  const convertMenuUrls: (menus: Menus) => Menus = useCallback((menus) => {
+  const convertedMenuUrls: Menus = useMemo(() => {
+    if (!import.meta.env.DEV) {
+      return menus;
+    }
+
     const newMenus = { ...menus };
     Object.keys(newMenus).forEach((menuSlug) => {
       newMenus[menuSlug].items = newMenus[menuSlug].items.map((item) => ({
         ...item,
-        guid: item.guid.replace(site.site_url, location.hostname),
-        url: item.url.replace(site.site_url, location.hostname),
+        guid: item.guid.replace(site.site_url, location.origin),
+        url: item.url.replace(site.site_url, location.origin),
       }));
     });
     return newMenus;
-  }, []);
+  }, [site, menus]);
 
-  const convertSiteUrls: (site: SiteOptions) => SiteOptions = useCallback(
-    (site) => {
-      const newSite = { ...site };
-      newSite.site_url = newSite.site_url.replace(
-        site.site_url,
-        location.hostname,
-      );
-      newSite.home_url = newSite.home_url.replace(
-        site.site_url,
-        location.hostname,
-      );
-      newSite.admin_ajax_url = newSite.admin_ajax_url.replace(
-        site.site_url,
-        location.hostname,
-      );
-      return newSite;
-    },
-    [],
-  );
+  const convertedSiteUrls: SiteOptions = useMemo(() => {
+    if (!import.meta.env.DEV) {
+      return site;
+    }
+
+    const newSite = { ...site };
+    newSite.site_url = newSite.site_url.replace(site.site_url, location.origin);
+    newSite.home_url = newSite.home_url.replace(site.site_url, location.origin);
+    newSite.admin_ajax_url = newSite.admin_ajax_url.replace(
+      site.site_url,
+      location.origin,
+    );
+    return newSite;
+  }, [site]);
 
   return {
     query,
-    site: convertSiteUrls(site),
+    site: convertedSiteUrls,
     translations,
     post,
-    menus: convertMenuUrls(menus),
+    menus: convertedMenuUrls,
     content,
   };
 };
